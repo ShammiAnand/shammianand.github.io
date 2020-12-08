0dcf6857ad118cb2181ba1d59027c8066025d7b8
Shammi Anand <shammianand101@gmail.com>
2020-12-08 17:11:49 +0530

    changes

---
+++ app.js
@@ -0,0 +1,12 @@
+// console.log("connected!");
+
+// const images = [
+//     "https://images.unsplash.com/photo-1607340385476-7f47b5700026?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80",
+//     "https://images.unsplash.com/photo-1606990972809-f6dff5d17367?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+//     "https://images.unsplash.com/photo-1603726629907-ba4600c520be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80"
+// ]
+// let img = images[Math.floor(Math.random() * 3)];
+
+// document.getElementById('main').style.backgroundImage = img;
+// document.getElementById('main').style.backgroundRepeat = "no-repeat";
+
